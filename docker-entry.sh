fc582bd8c8e96bb237534ecb9f675d67f21b63b4
SladerZ <SladerZ@users.noreply.github.com>
2016-06-29 13:23:27 +0200

    Update docker-entry.sh

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,4 +1,4 @@
 echo Starting Owncloud
 ln -sv /etc/nginx/sites-available/owncloud.conf /etc/nginx/sites-enabled/owncloud
-#sed -i '/true,/a\  '"'memcache.local' => '\OC\Memcache\APC',"'' ./owncloud/config/config.php
+
 service php5-fpm start && nginx
