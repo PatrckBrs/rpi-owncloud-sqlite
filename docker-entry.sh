3f9a9cfc3d21f90b65fbd77510e92940cbffe629
SladerZ <SladerZ@users.noreply.github.com>
2016-06-29 13:56:33 +0200

    Update docker-entry.sh

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,4 +1,10 @@
 echo Starting Owncloud
 ln -sv /etc/nginx/sites-available/owncloud.conf /etc/nginx/sites-enabled/owncloud
 
+[[ -f /var/www/owncloud/config/config.php ]] && {
+    grep -q APCu /var/www/owncloud/config/config.php ||
+        sed -i '/^);/i\  '"'memcache.local' => '\\\\OC\\\\Memcache\\\\APCu'," \
+                    /var/www/owncloud/config/config.php
+}
+
 service php5-fpm start && nginx
