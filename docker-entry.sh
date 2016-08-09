ca12bbd40cac179e7c3e8958649831e564376379
Patrck.Brs <PatrckBrs@users.noreply.github.com>
2016-08-09 15:09:03 +0200

    Update docker-entry.sh

--- docker-entry.sh
+++ docker-entry.sh
@@ -1,10 +1,9 @@
 echo Starting Owncloud
 ln -sv /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
-[[ -f /var/www/owncloud/config/config.php ]] && {
-    grep -q APCu /var/www/owncloud/config/config.php ||
-        sed -i '/^);/i\  '"'memcache.local' => '\\\\OC\\\\Memcache\\\\APCu'," \
-                    /var/www/owncloud/config/config.php
-}
+#[[ -f /var/www/owncloud/config/config.php ]] && {
+#    grep -q APCu /var/www/owncloud/config/config.php ||
+sed -i '/^);/i\  '"'memcache.local' => '\\\\OC\\\\Memcache\\\\APCu'," /var/www/owncloud/config/config.php
+#}
 
 service php5-fpm start && nginx
