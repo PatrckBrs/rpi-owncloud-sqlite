89a0eb3c159b631960dc5e27121986e3abb14430
Patrck.Brs <PatrckBrs@users.noreply.github.com>
2016-09-15 20:21:21 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ -f "/var/www/owncloud/config/config.php" ]
+then
+  sed -i '/^);/i\  '"'memcache.local' => '\\\\OC\\\\Memcache\\\\APCu'," /var/www/owncloud/config/config.php
+fi
+
+/usr/bin/supervisord -n -c /etc/supervisord.conf
