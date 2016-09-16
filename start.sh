033c162640cb00b9cfb9d40350dd82b43ee42c38
Patrck.Brs <PatrckBrs@users.noreply.github.com>
2016-09-16 13:09:49 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,5 +4,3 @@ if [ -f "/var/www/owncloud/config/config.php" ]
 then
   sed -i '/^);/i\  '"'memcache.local' => '\\\\OC\\\\Memcache\\\\APCu'," /var/www/owncloud/config/config.php
 fi
-
-/usr/bin/supervisord -n -c /etc/supervisord.conf
