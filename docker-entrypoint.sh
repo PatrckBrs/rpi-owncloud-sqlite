7898cc9221078e1735be5257e32713495868f6c3
Patrck.Brs <PatrckBrs@users.noreply.github.com>
2016-09-16 22:47:59 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,3 +4,4 @@ if [ -f "/var/www/owncloud/config/config.php" ]
 then
   sed -i '/^);/i\  '"'memcache.local' => '\\\\OC\\\\Memcache\\\\APCu'," /var/www/owncloud/config/config.php
 fi
+/usr/bin/supervisord -n -c /etc/supervisord.conf
