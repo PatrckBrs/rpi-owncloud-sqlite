0c3a901b412b853ff1658615ab37c35e32c8b735
Patrck.Brs <PatrckBrs@users.noreply.github.com>
2016-10-09 20:36:08 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-MEM="memcache.local"
+MEM=$(grep memcache /var/www/owncloud/config/config.php  | wc -l)
 
 if [ -f "/var/www/owncloud/config/config.php" ]
 then
-if  [ -z "$MEM" ]
+if  [[ "$MEM" -eq "0" ]]
 then
-  sed -i '/^);/i\  '"'memcache.local' => '\\\\OC\\\\Memcache\\\\APCu'," /var/www/owncloud/config/config.php
+  sed -i '/^);/i\  '"'memcache.local' => '\\\\OC\\\\Memcache\\\\APCu'," /var/www
+/owncloud/config/config.php
+else
+echo "OK"
   fi
 fi
+
 /usr/bin/supervisord -n -c /etc/supervisord.conf
